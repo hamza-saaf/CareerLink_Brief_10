e164e29fde2b3af46092276028dce81628bae09e
Hamza Saaf <hamzasaaf725@gmail.com>
2025-01-06 11:17:55 +0100

    Creation la Classe DataBase pour connecter la base de donnees

--- src/config/Database.php
+++ src/config/Database.php
@@ -1,1 +1,22 @@
-<?php  ?>
+<?php
+class Database{  
+
+    $servername= "localhost";
+    $dbname="careerlink_db";
+    $username = "username";
+    $password = "";
+
+    try{
+        $db= new PDO("mysql:host=$localhost;dbname=careerlink_db",$username,$password);
+    
+    }
+    catch(PDOException $e){
+        echo "<p>Erreur:".$e->getMessage();
+        die();
+    
+    }
+
+}
+
+
+?>
